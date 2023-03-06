647ecbe14579edf9d1ae91e305923132b1c6b16e
atomic928 <64305950+atomic928@users.noreply.github.com>
2023-03-06 11:24:10 +0900

    statusの仕様決めた

--- src/api/internal/model/location/table.go
+++ src/api/internal/model/location/table.go
@@ -6,5 +6,8 @@ type Location struct {
 	RelativeTime string  `db:"relative_time"`
 	Latitude     float64 `db:"latitude"`
 	Longitude    float64 `db:"longitude"`
+	// 鬼か逃げか: 鬼: 1, 逃げ: 2
+	// 人か罠か: 人: 1, 罠: 2、罠削除: 3
+	// 2桁で送る
 	Status       int     `db:"status"`
 }
--- src/api/internal/model/player/table.go
+++ src/api/internal/model/player/table.go
@@ -5,5 +5,8 @@ type Player struct {
 	SecretWords  string  `db:"secret_words"`
 	Name         string  `db:"name"`
 	Icon         int     `db:"icon"`
+	// 鬼か逃げか: 鬼: 1, 逃げ: 2
+	// 普通: 1, トラップにかかった: 2, 捕まった: 3, 逃げ切った: 4
+	// 2桁で送る
 	Status       int     `db:"status"`
 }
